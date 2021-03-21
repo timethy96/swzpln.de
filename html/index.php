41be993460fa179deeb54c0ad3ed308247cf9546
timethy96 <timo@bilhoefer.de>
2021-03-21 16:25:04 +0100

    otf error resolution (reproject not found)

--- html/index.php
+++ html/index.php
@@ -91,24 +91,15 @@ if ($fr) {
         E-Mail: swzpln ø bilhoefer · de</br></p><p><strong>Haftungsausschluss: </strong><br><br><strong>Haftung für Inhalte</strong><br><br>
         Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br><br><strong>Haftung für Links</strong><br><br>
         Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br><br><strong>Urheberrecht</strong><br><br>
-	Die durch die Seitenbetreiber erstellten Inhalte, Werke und Code auf diesen Seiten unterliegen der <a href="https://opensource.org/licenses/AGPL-3.0" target="_blank">AGPL-3.0 - Lizenz</a>. Die Downloads werden Lizenzlos (<a href="https://creativecommons.org/publicdomain/zero/1.0/deed.de" target="_blank">CC0</a>) bereitgestellt, die Kartendaten stehen unter der <a href="https://osm.org/copyright" target="_blank">OpenStreetMaps-Lizenz</a>. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br><br><strong>Datenschutz</strong><br><br>
-        Die Nutzung unserer Webseite ist ohne Angabe personenbezogener Daten möglich. Kein "wenn" und "aber". <br>
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.<br><br><strong>Datenschutz</strong><br><br>
+        Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. <br>
         Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. <br>
         Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.<br>
         </p><br> 
-        Website Impressum teilweise von <a href="https://www.impressum-generator.de">impressum-generator.de</a>
+        Website Impressum von <a href="https://www.impressum-generator.de">impressum-generator.de</a>
  
         <p>Kartendaten: &copy; <?php echo date("Y"); ?> <a target="_blank" href="https://osm.org/copyright">OpenStreetMap</a> contributors</p>
         <p>Overpass-API bereitgestellt von <a target="_blank" href="https://overpass.kumi.systems/">kumi.systems</a></p>
-        <p>Used Libraries:<br/>
-               <a href="https://jquery.com/" target="blank">jQuery</a>: MIT-License<br/>
-               <a href="https://leafletjs.com/" target="blank">Leaflet-Map</a>: BSD2-License<br/>
-               <a href="https://github.com/tyrasd/osmtogeojson" target="blank">osmtogeojson.js</a>: MIT-License<br/>
-               <a href="https://github.com/gagan-bansal/geojson2svg" target="blank">geojson2svg.js</a>: MIT-License<br/>
-               <a href="https://github.com/geosquare/reproject-spherical-mercator" target="blank">reproject-spherical-mercator.js</a>: MIT-License<br/>
-               <a href="https://github.com/MrRio/jsPDF" target="blank">jsPDF</a>: MIT-License<br/>
-               <a href="https://github.com/yWorks/svg2pdf.js" target="blank">svg2pdf</a>: MIT-License<br/>
-        </p>
         <br/><br/><br/><br/>
 
     </div>
@@ -117,7 +108,7 @@ if ($fr) {
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
     <script src='js/osmtogeojson.js'></script>
     <script src='js/geojson2svg.min.js'></script>
-    <script src='js/repoject-spherical-mercator.min.js'></script>
+    <script src='js/reproject.js'></script>
     <script src='js/browser.maker.js'></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.0.0/jspdf.umd.min.js" integrity="sha512-g77bZKU4ktH2I5nNioWzOMcbd3fN/svB0vQM73Uo5GRn/EGfcSJB0DlR1ithxnFsDaa0HmlOwHYiUFeM1P3RRw==" crossorigin="anonymous"></script>
     <script src="js/svg2pdf.umd.min.js"></script>
