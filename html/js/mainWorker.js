157f3adfdf28fbef47fa6b3fce0c7e8674b4e989
timethy96 <timo@bilhoefer.de>
2021-04-26 23:02:08 +0200

    BUGFIX: Multiple Workers are working now with single lines only

--- html/js/mainWorker.js
+++ html/js/mainWorker.js
@@ -44,48 +44,40 @@ onmessage = function(e) {
         var gjson = osmtogeojson(osm);
         var mgjson = reproject(gjson);
 
-        /* var allSteps = Math.ceil(mgjson.features.length / 500);
-        if (allSteps > 8){
-            var allSteps = 8;
-        } */
-        var allSteps = 1;
-        var oneStep = mgjson.features.length / allSteps;
+        var workersLen = Math.ceil(mgjson.features.length / 1000);
+        if (workersLen > 8){
+            var workersLen = 8;
+        };
+        var oneStep = Math.ceil(mgjson.features.length / workersLen);
 
-        var gjWorkers = [];
-        for (var i=0; i < allSteps; i+=1){
+        allWorkers = [];
+        resultArray = [];
+
+        for (var i=0; i < mgjson.features.length; i += oneStep){
             newDict = {"type":"FeatureCollection", "features":""};
             newDict.features = mgjson.features.slice(i,i+oneStep);
-            gjWorkers.push(newDict);
+            var worker = new Worker('subWorker.js');
+            worker.postMessage([newDict, mlonA, mlatA, mlonB, mlatB, widthMeters, heightMeters, thisID])
+            allWorkers.push(worker);
         }
-
-        workersLen = gjWorkers.length;
 
         postMessage(["setLBar", 60, "GeoJSON in SVG konvertieren..."]);
 
-        allWorkers = []
-        resultArray = []
 
-        gjWorkers.forEach(gjArray => {
-            var worker = new Worker('subWorker.js');
-            worker.postMessage([gjArray, mlonA, mlatA, mlonB, mlatB, widthMeters, heightMeters, thisID])
-            allWorkers.push(worker);
-        });
-
-        allWorkers.forEach(worker => {
-            worker.onmessage = function(e){
-                if (e.data[0] == "setLBar"){
-                    postMessage(["setLBar", e.data[1], e.data[2]]);
+        allWorkers.forEach(workerR => {
+            workerR.onmessage = function(f){
+                if (f.data[0] == "setLBar"){
+                    postMessage(["setLBar", f.data[1], f.data[2]]);
                 } else {
-                    resultArray.push(e.data[1]);
-                    console.log("pushing worker data: " + e.data[1]);
+                    resultArray.push(f.data[1]);
                     if (resultArray.length == workersLen){
-                        if (e.data[0] == "svg") {
+                        if (f.data[0] == "svg") {
                             postMessage(["setLBar", 80, "SVG-Datei erstellen..."]);
-                            var svg = resultArray.join('');
+                            var svg = resultArray.join("");
                             var svgFile = `<svg version="1.1" baseProfile="full" width="${widthMeters}mm" height="${heightMeters}mm" xmlns="http://www.w3.org/2000/svg">` + svg + '</svg>';
                             postMessage(["setLBar", 100, "Download starten..."]);
                             postMessage(["download", "svg", svgFile]);
-                        } else if (e.data[0] == "dxf") {
+                        } else if (f.data[0] == "dxf") {
                             importScripts('browser.maker.js');
                             var makerjs = require('makerjs');
                             var modelDict = {"models":{}};
@@ -98,7 +90,7 @@ onmessage = function(e) {
                             var dxfString = makerjs.exporter.toDXF(modelDict, {"units":"meter","usePOLYLINE":true});
                             postMessage(["setLBar", 100, "Download starten..."]);
                             postMessage(["download", "dxf", dxfString]);
-                        } else if (e.data[0] == "pdf") {
+                        } else if (f.data[0] == "pdf") {
                             var svg = resultArray.join('');
                             var svgFile = `<svg version="1.1" baseProfile="full" width="${widthMeters * 3.7795}" height="${heightMeters * 3.7795}" xmlns="http://www.w3.org/2000/svg">` + svg + '</svg>';
                             postMessage(["download", "pdf", svgFile, widthMeters, heightMeters]);
