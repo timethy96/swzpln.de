b2a4bedcf113cd7934b232dd2b9a774dec6af254
timethy96 <timo@bilhoefer.de>
2021-10-28 11:48:55 +0200

    fix generation on safari (error receiving all messages from subworkers into main function)

--- html/js/mainWorker.js
+++ html/js/mainWorker.js
@@ -108,83 +108,84 @@ const style = `
 
 
 onmessage = function(e) {
+    
+    if (! e.data._from) {
+        const [thisID,latA,lonA,latB,lonB,mlatA,mlonA,mlatB,mlonB,heightMeters,widthMeters,overpassApi, dataArray] = e.data;
 
-    const [thisID,latA,lonA,latB,lonB,mlatA,mlonA,mlatB,mlonB,heightMeters,widthMeters,overpassApi, dataArray] = e.data
+        getOSMdata(overpassApi,latA,lonA,latB,lonB,dataArray, function(osm){
+            
+            postMessage(["setLBar", 40, "OSMXML in GeoJSON konvertieren..."]);
 
-    getOSMdata(overpassApi,latA,lonA,latB,lonB,dataArray, function(osm){
-        
-        postMessage(["setLBar", 40, "OSMXML in GeoJSON konvertieren..."]);
+            //convert osmxml to geojson for further processing 
+            const gjson = osmtogeojson(osm);
+            const mgjson = reproject(gjson);
 
-        //convert osmxml to geojson for further processing 
-        const gjson = osmtogeojson(osm);
-        const mgjson = reproject(gjson);
+            console.log(mgjson);
 
-        console.log(mgjson);
+            var workersLen = Math.ceil(mgjson.features.length / 1000);
+            if (workersLen > 8){
+                var workersLen = 8;
+            };
+            var oneStep = Math.ceil(mgjson.features.length / workersLen);
 
-        var workersLen = Math.ceil(mgjson.features.length / 1000);
-        if (workersLen > 8){
-            var workersLen = 8;
-        };
-        var oneStep = Math.ceil(mgjson.features.length / workersLen);
+            allWorkers = [];
+            resultArray = [];
 
-        allWorkers = [];
-        resultArray = [];
+            for (var i=0; i < mgjson.features.length; i += oneStep){
+                newDict = {"type":"FeatureCollection", "features":""};
+                newDict.features = mgjson.features.slice(i,i+oneStep);
+                var worker = new Worker('subWorker.js');
+                worker.postMessage([newDict, mlonA, mlatA, mlonB, mlatB, widthMeters, heightMeters, thisID])
+                allWorkers.push(worker);
+            }
 
-        for (var i=0; i < mgjson.features.length; i += oneStep){
-            newDict = {"type":"FeatureCollection", "features":""};
-            newDict.features = mgjson.features.slice(i,i+oneStep);
-            var worker = new Worker('subWorker.js');
-            worker.postMessage([newDict, mlonA, mlatA, mlonB, mlatB, widthMeters, heightMeters, thisID])
-            allWorkers.push(worker);
-        }
-
-        postMessage(["setLBar", 60, "GeoJSON in SVG konvertieren..."]);
+            postMessage(["setLBar", 60, "GeoJSON in SVG konvertieren..."]);
 
 
-        allWorkers.forEach(workerR => {
-            workerR.onmessage = function(f){
-                if (f.data[0] == "setLBar"){
-                    postMessage(["setLBar", f.data[1], f.data[2]]);
-                } else {
-                    resultArray.push(f.data[1]);
-                    if (resultArray.length == workersLen){
-                        if (f.data[0] == "svg") {
-                            postMessage(["setLBar", 80, "SVG-Datei erstellen..."]);
-                            var svg = resultArray.join("");
-                            var svgFile = `<svg version="1.1" baseProfile="full" width="${widthMeters}mm" height="${heightMeters}mm" xmlns="http://www.w3.org/2000/svg">` + style + svg + '</svg>';
-                            postMessage(["setLBar", 100, "Download starten..."]);
-                            postMessage(["download", "svg", svgFile]);
-                        } else if (f.data[0] == "dxf") {
-                            importScripts('browser.maker.js');
-                            var makerjs = require('makerjs');
-                            var modelDict = {"models":{}};
-                            var i = 0;
-                            resultArray.forEach(element => {
-                                modelDict["models"][i] = element;
-                                i++;
-                            });
-                            postMessage(["setLBar", 95, "DXF-Datei erstellen..."]);
-                            var dxfString = makerjs.exporter.toDXF(modelDict, {"units":"meter","usePOLYLINE":true});
-                            postMessage(["setLBar", 100, "Download starten..."]);
-                            postMessage(["download", "dxf", dxfString]);
-                        } else if (f.data[0] == "pdf") {
-                            var svg = resultArray.join('');
-                            var svgFile = `<svg version="1.1" baseProfile="full" width="${widthMeters * 3.7795}" height="${heightMeters * 3.7795}" xmlns="http://www.w3.org/2000/svg">` + style + svg + '</svg>';
-                            postMessage(["download", "pdf", svgFile, widthMeters, heightMeters]);
-                        }
-                    } 
+            allWorkers.forEach(workerR => {
+                workerR.onmessage = function(f){
+                    if (f.data[0] == "setLBar"){
+                        postMessage(["setLBar", f.data[1], f.data[2]]);
+                    } else {
+                        resultArray.push(f.data[1]);
+                        if (resultArray.length == workersLen){
+                            if (f.data[0] == "svg") {
+                                postMessage(["setLBar", 80, "SVG-Datei erstellen..."]);
+                                var svg = resultArray.join("");
+                                var svgFile = `<svg version="1.1" baseProfile="full" width="${widthMeters}mm" height="${heightMeters}mm" xmlns="http://www.w3.org/2000/svg">` + style + svg + '</svg>';
+                                postMessage(["setLBar", 100, "Download starten..."]);
+                                postMessage(["download", "svg", svgFile]);
+                            } else if (f.data[0] == "dxf") {
+                                importScripts('browser.maker.js');
+                                var makerjs = require('makerjs');
+                                var modelDict = {"models":{}};
+                                var i = 0;
+                                resultArray.forEach(element => {
+                                    modelDict["models"][i] = element;
+                                    i++;
+                                });
+                                postMessage(["setLBar", 95, "DXF-Datei erstellen..."]);
+                                var dxfString = makerjs.exporter.toDXF(modelDict, {"units":"meter","usePOLYLINE":true});
+                                postMessage(["setLBar", 100, "Download starten..."]);
+                                postMessage(["download", "dxf", dxfString]);
+                            } else if (f.data[0] == "pdf") {
+                                var svg = resultArray.join('');
+                                var svgFile = `<svg version="1.1" baseProfile="full" width="${widthMeters * 3.7795}" height="${heightMeters * 3.7795}" xmlns="http://www.w3.org/2000/svg">` + style + svg + '</svg>';
+                                postMessage(["download", "pdf", svgFile, widthMeters, heightMeters]);
+                            }
+                        } 
+                    }
+                    
                 }
                 
-            }
+            });
+            
+                    
+
             
         });
-        
-                
 
-        
-    });
-
-            
+    };        
     
     
 };
