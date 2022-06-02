805cffa51b12258843f4ddf7356d7d657b25c166
timethy96 <timo@bilhoefer.de>
2022-06-02 18:28:30 +0200

    fix mirrored svg & referencing problem

--- html/js/osm/osmjson2svg.js
+++ html/js/osm/osmjson2svg.js
@@ -1,4 +1,4 @@
-importScripts('./osmjson2objarray.js', './svg.min.js');
+importScripts('./osmjson2objarray.js');
 //import { osmjson2objarray, deg2XY } from './osmjson2objarray.js'; //-> module webworkers not yet implemented in Firefox
 
 //export function osmjson2dxf(osm_json, bounds, layers, progressCallback) { //-> module webworkers not yet implemented in Firefox
@@ -19,23 +19,28 @@ function osmjson2svg(osm_json, bounds, layers, zoom, progressCallback) {
         other: '#FF0000',
         contours: 'none'
     }
-
+    points='"+path.toString()+"' 
     let SE = deg2XY(bounds, bounds[0], bounds[1]);
     let NW = deg2XY(bounds, bounds[2], bounds[3]);
     let txtSize = (19 - zoom) * 10;
 
     var Drawing = "<svg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' width='"+SE[0]+"' height='"+NW[1]+"'>"
-
-    //Drawing.text("(c) OpenStreetMap.org contributors").font({anchor: 'right', size: txtSize});
+    
+    Drawing += "<text x='"+SE[0]+"' y='"+NW[1]+"' text-anchor='end' style='font: "+txtSize+"px sans-serif;'>(c) OpenStreetMap.org contributors</text>"
 
     i=0;
     objects.forEach((obj) => {
         let type = obj['type'];
         let path = obj['path'];
+        for (let c = 0; c < path.length; c++) {
+            path[c][1] = (path[c][1] - NW[1]) * -1; //svg has Y values from top to bottom (dxf:bottom-top) --> reverse Y values
+        }
+        
         if (['highway','railway','contours'].includes(type)) {
             Drawing += "<polyline points='"+path.toString()+"' style='fill:none;stroke:"+layerColors[type]+"' />"
         } else {
             Drawing += "<polygon points='"+path.toString()+"' style='fill:"+layerColors[type]+"' />"
+         
         }
 
         //callback
