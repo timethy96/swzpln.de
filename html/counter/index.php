ef8650fc158fd792d6fdce6959872490d9032b77
timethy96 <timo@bilhoefer.de>
2022-07-07 17:37:18 +0200

    allow counter cross page request for all versions

--- html/counter/index.php
+++ html/counter/index.php
@@ -1,4 +1,19 @@
 <?php
+$allowedOrigins = array(
+    '(http(s)://)?(www\.)?swzpln.de'
+  );
+   
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+    foreach ($allowedOrigins as $allowedOrigin) {
+        if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Methods: GET, OPTIONS');
+        header('Access-Control-Max-Age: 1000');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+        break;
+        }
+    }
+}
 
 error_reporting(0);
 
