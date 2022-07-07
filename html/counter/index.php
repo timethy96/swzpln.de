76cf53af5f017bd7b484501ac8bcf196439f4123
timethy96 <timo@bilhoefer.de>
2022-07-07 17:55:33 +0200

    allow counter cross page request for all versions 7

--- html/counter/index.php
+++ html/counter/index.php
@@ -1,20 +1,5 @@
 <?php
-$allowed_domains = array(
-    'https://swzpln.de',
-    'https://www.swzpln.de',
-    'https://old.swzpln.de',
-    '172.18.0.2'
-);
-if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
-    $origin = $_SERVER['HTTP_ORIGIN'];
-} else if (array_key_exists('HTTP_REFERER', $_SERVER)) {
-    $origin = $_SERVER['HTTP_REFERER'];
-} else {
-    $origin = $_SERVER['REMOTE_ADDR'];
-}
-if (in_array($origin, $allowed_domains)) {
-    header('Access-Control-Allow-Origin: ' . $origin);
-}
+header('Access-Control-Allow-Origin: *');
 
 error_reporting(0);
 
