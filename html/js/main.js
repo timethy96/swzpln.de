aa483c3629631e80b7349e52f68ddb22c4d7cd1b
timethy96 <timo@bilhoefer.de>
2022-07-08 11:37:53 +0200

    allow counter cross page request for all versions 11

--- html/js/main.js
+++ html/js/main.js
@@ -176,7 +176,7 @@ function download(filename, text, mime) {
 // trigger the counter
 
 function countUp(){
-    $.ajax({url: "https://swzpln.de/counter?count=1"});
+    $.ajax({url: "https://swzpln.de/counter/?count=1"});
 }
 
 
