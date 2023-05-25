f8248793b05822822f7c493da3fe292ba7fc8da0
timethy96 <timo@bilhoefer.de>
2023-05-25 19:01:07 +0200

    disable cache for counter

--- html/c/index.php
+++ html/c/index.php
@@ -3,10 +3,15 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Max-Age: 600');
 header('Access-Control-Allow-Headers: *');
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT"); // *
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 error_reporting(0);
 
-$db = new SQLite3('db/counter.db');
+$db = new SQLite3('./db/counter.db');
 if (!$db) {
     echo $db->lastErrorMsg();
 } else {
