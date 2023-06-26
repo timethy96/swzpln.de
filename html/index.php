08b243d8e9841b3b19a91e72d1b0e989ae5f0cfb
timethy96 <timo@bilhoefer.de>
2023-06-26 07:54:16 +0200

    add canonical sitetag for search slug

--- html/index.php
+++ html/index.php
@@ -86,7 +86,7 @@ if (isset($_GET['url'])){
   <link rel="manifest" href="/meta/site.webmanifest">
   <link rel="mask-icon" href="/meta/safari-pinned-tab.svg" color="#000000">
   <link rel="shortcut icon" href="/meta/favicon.ico">
-  <link rel="canonical" href="https://<?php echo $server_name ?>">
+  <link rel="canonical" href="https://<?php echo $server_name."/".$searchString ?>">
   <meta name="apple-mobile-web-app-title" content="<?php echo $server_name ?>">
   <meta name="application-name" content="<?php echo $server_name ?>">
   <meta name="msapplication-TileColor" content="#000000">
